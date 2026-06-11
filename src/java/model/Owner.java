bd6f065bd5d10b13e4c124446afad82754396883
nalndra <jatayu2211@gmail.com>
2026-06-11 14:46:42 +0700

    owners & verif

--- src/java/model/Owner.java
+++ src/java/model/Owner.java
@@ -8,6 +8,35 @@ package model;
  *
  * @author Lenovo
  */
-public class Owner {
+public class Owner extends User {
+
     
+    public Owner() {
+        super();
+    }
+
+    
+    public Owner(String userId, String name, String email, String password, String phone, String role) {
+        super(userId, name, email, password, phone, role);
+    }
+
+    public void viewReport() {
+        System.out.println("[Owner] " + getName() + " (ID: " + getUserId() + ") melihat daftar laporan keluhan masuk.");
+    }
+
+    public void addProperty(String propertyName) {
+        System.out.println("[Owner] " + getName() + " (ID: " + getUserId() + ") mendaftarkan properti baru: " + propertyName);
+    }
+
+    public void viewProperty() {
+        System.out.println("[Owner] " + getName() + " (ID: " + getUserId() + ") melihat daftar properti miliknya.");
+    }
+
+    public void editProperty(int propertyId) {
+        System.out.println("[Owner] " + getName() + " (ID: " + getUserId() + ") mengubah data properti ID: " + propertyId);
+    }
+
+    public void deleteProperty(int propertyId) {
+        System.out.println("[Owner] " + getName() + " (ID: " + getUserId() + ") menghapus properti ID: " + propertyId);
+    }
 }
--- src/java/model/Verification.java
+++ src/java/model/Verification.java
@@ -3,11 +3,63 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package model;
-
+import java.util.Date;
 /**
  *
  * @author Lenovo
  */
 public class Verification {
+
+    private int verificationId;
+    private Property property;
+    private String status;
+    private Date date;
+    public Verification() {}
+
+    public Verification(int verificationId, Property property, String status, Date date) {
+        this.verificationId = verificationId;
+        this.property = property;
+        this.status = status;
+        this.date = date;
+    }
+
+    public int getVerificationId() {
+        return verificationId;
+    }
+
+    public void setVerificationId(int verificationId) {
+        this.verificationId = verificationId;
+    }
+
+    public Property getProperty() {
+        return property;
+    }
+
+    public void setProperty(Property property) {
+        this.property = property;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
     
+    public void approve() {
+        status = "Approved";
+    }
+
+    public void reject() {
+        status = "Rejected";
+    }
 }
